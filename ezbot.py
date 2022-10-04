345236f4bf6ac3a6f39d036d157e4c0c8162af24
Arnav <arnanduri2006@gmail.com>
2022-10-03 22:28:31 -0400

    Made sample commands more user-friendly

--- ezbot.py
+++ ezbot.py
@@ -12,11 +12,11 @@ g.generateFiles("MTAyNjUwNDQzNDkzMjUyNzE2NA.GHN8gF.P8k8grbz0we-rhaPNZnEhGED5uQL4
 for group in command_groups:
     g.create_cog_base(group)
     if group == "SampleCommandGroup1":
-        g.add_cog_command(group, "test1", "Sends test1", "", """await interaction.response.send_message(\"test1\")""")
+        g.add_cog_command(group, "test1", "Sends a welcome message", "", """await interaction.response.send_message(\"Welcome to EZBot!\")""")
     if group == "SampleCommandGroup2":
-        g.add_cog_command(group, "test2", "Sends test2 followed by your message", ["message: str"], """await interaction.response.send_message(\"test2 \" + message)""")
+        g.add_cog_command(group, "test2", "Sends Hello, followed by your name!", ["name: str"], """await interaction.response.send_message(\"Hello,  \" + name + \"!\")""")
     if group == "SampleCommandGroup3":
-        g.add_cog_command(group, "test3", "Sends test3 followed by your name and age", ["name: str", "age: int"], """await interaction.response.send_message(\"test3 \" + name + \", \" + str(age))""")
+        g.add_cog_command(group, "test3", "Sends Hello, followed by your name and age", ["name: str", "age: int"], """await interaction.response.send_message(\"Hello, \" + name + \", I believe you are \" + str(age) + \" years old!\")""")
     g.close_cog(group, "1026505045501554730")
 
 
